c7174fc212c84b4bad51338d7d65865b1aea29a5
kongmongthao <kongmongthao348@gmail.com>
2026-07-15 17:37:48 +0700

    update version5

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = config.get<number>('port') || 3000;
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
   // eslint-disable-next-line no-console
   console.log(`🚀 HR_App backend running on http://localhost:${port}/api`);
   // eslint-disable-next-line no-console
